a5fa05bbfa1dfbf2c6eda51a53d20e63230212b7
cristian ayala <cristian.ayala.064@gmail.com>
2022-10-12 12:56:12 -0500

    initial tests

---
+++ PruebaiOSTests/List/ListPresenterTest.swift
@@ -0,0 +1,22 @@
+//
+//  ListPresenterTest.swift
+//  PruebaiOSTests
+//
+//  Created by Cristian Ayala on 12/10/22.
+//
+
+import Foundation
+
+import XCTest
+@testable import PruebaiOS
+
+class ListPresenterTest: XCTestCase {
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+}
