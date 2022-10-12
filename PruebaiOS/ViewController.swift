244f580b3e303ca62bd099c3cab64706adb97099
cristian ayala <cristian.ayala.064@gmail.com>
2022-10-11 22:57:56 -0500

    Initial Commit

---
+++ PruebaiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaiOS
+//
+//  Created by Cristian Ayala on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
